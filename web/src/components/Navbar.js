86d85064c84e74b3087e795cb21a8d1c119778c7
danvar03 <daniela.03v@gmail.com>
2021-10-20 12:36:35 -0500

    Se agrega favicon y  logo

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../img/logo.png'
+
 
 export const PublicNavbar = () => (
-  <nav>
+  <Fragment>     
+  <nav>   
     <section>
       <Link to="/">Home</Link>
       <Link to="/questions">Questions</Link>
     </section>
   </nav>
+  <div className= "logo">  <img src={logo}  alt="logo" /></div>
+  
+  </Fragment>
 )
 
 export const PrivateNavbar = () => (
   <nav>
+<img src='/public/img/favicon.png' className="App-logo" alt="logo" />
     <section>
+    
       <Link to="/">Home</Link>
       <Link to="/questions">Questions</Link>
       <Link to="/new">New</Link>
