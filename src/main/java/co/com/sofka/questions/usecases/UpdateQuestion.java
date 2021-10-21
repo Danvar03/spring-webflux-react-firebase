b377118c121d101ca214e3c29742d90a86de4d4c
danvar03 <daniela.03v@gmail.com>
2021-10-21 13:44:44 -0500

    Se agrega en el back calificacion

--- src/main/java/co/com/sofka/questions/usecases/UpdateQuestion.java
+++ src/main/java/co/com/sofka/questions/usecases/UpdateQuestion.java
@@ -1,4 +1,12 @@
 package co.com.sofka.questions.usecases;
 
-public class UnpdateQuestion {
+
+import co.com.sofka.questions.model.QuestionDTO;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@FunctionalInterface
+public interface UpdateQuestion {
+    Mono<QuestionDTO> apply(@Valid QuestionDTO questionDTO);
 }
