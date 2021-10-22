70986b129641063b2788f397598da1683d2c0956
danvar03 <daniela.03v@gmail.com>
2021-10-22 00:13:40 -0500

    Se avanza en  servicio para actualizar usuario en backend

---
+++ src/main/java/co/com/sofka/questions/collections/User.java
@@ -0,0 +1,46 @@
+package co.com.sofka.questions.collections;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+    @Id
+    private String userId;
+    private String name;
+    private String lastName;
+    private String email;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/co/com/sofka/questions/reposioties/UserRepository.java
@@ -0,0 +1,9 @@
+package co.com.sofka.questions.reposioties;
+
+import co.com.sofka.questions.collections.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends ReactiveCrudRepository<User, String> {
+}
